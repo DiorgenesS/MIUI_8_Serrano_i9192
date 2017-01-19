3c006ddb4fadc6d9a2d3f745850772a53d031fed
Diorgenes <dyogenes_gd@hotmail.com>
2017-01-19 00:42:07 -0300

    updated releasetools.py

--- releasetools.py
+++ releasetools.py
@@ -5,15 +5,17 @@ def RemoveDeviceAssert(info):
   edify = info.script
   for i in xrange(len(edify.script)):
     if "ro.product" in edify.script[i]:
-      edify.script[i] = ''
+      edify.script[i] = """assert(getprop("ro.product.device") == "serranods" || getprop("ro.build.product") == "serranods" || getprop("ro.product.device") == "serranodsdd" || getprop("ro.build.product") == "serranodsdd" || getprop("ro.product.device") == "serranodsub" || getprop("ro.build.product") == "serranodsub" || getprop("ro.product.device") == "serranodsxx" || getprop("ro.build.product") == "serranodsxx" || getprop("ro.product.device") == "GT-I9192" || getprop("ro.build.product") == "GT-I9192" || abort("This package is for device: serranods,serranodsdd,serranodsub,serranodsxx,GT-I9192; this device is " + getprop("ro.product.device") + "."););
+unmount("/data");
+unmount("/system");"""
       return
 
 def AddAssertions(info):
-    edify = info.script
-    for i in xrange(len(edify.script)):
-        if " ||" in edify.script[i] and ("ro.product.device" in edify.script[i] or "ro.build.product" in edify.script[i]):
-            edify.script[i] = edify.script[i].replace(" ||", ' || getprop("ro.build.product") == "serranods" || getprop("ro.product.device") == "serranodsub" || getprop("ro.build.product") == "serranodsub" || getprop("ro.product.device") == "serranodsdd" || getprop("ro.build.product") == "serranodsdd" || getprop("ro.product.device") == "serranodsxx" || getprop("ro.build.product") == "serranodsxx" || getprop("ro.product.device") == "GT-I9192" || getprop("ro.build.product") == "GT-I9192" ||')
-            return
+   edify = info.script
+   for i in xrange(len(edify.script)):
+    if " ||" in edify.script[i] and ("ro.product.device" in edify.script[i] or "ro.build.product" in edify.script[i]):
+      edify.script[i] = edify.script[i].replace(" ||", ' ')
+      return
 
 def AddArgsForFormatSystem(info):
   edify = info.script
@@ -31,7 +33,7 @@ def WritePolicyConfig(info):
 
 def FullOTA_InstallEnd(info):
     WritePolicyConfig(info)
-    AddAssertions(info)
+    RemoveDeviceAssert(info)
 
 def IncrementalOTA_InstallEnd(info):
-    AddAssertions(info)
+    RemoveDeviceAssert(info)
